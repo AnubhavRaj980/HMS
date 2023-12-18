2597b5c66388a59761e23c9d581a93229cb8b8cc
San123san <s35102751@gmail.com>
2023-12-18 22:34:31 +0530

    Initial Commit

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import logo from '../../Images/logo.jpg'
+const Navbar = () => {
+    return (
+        <div>
+
+            {/* ----------------------------------Navbar----------------------------- */}
+            <div>
+                <div className='flex items-center space-around space-x-4 bg-gray-300'>
+
+                    {/* --------------------------------------Logo--------------------------- */}
+                    {/* <div className="flex-initial w-59 p-4">Logo</div> */}
+                    <img src={logo} alt="logo_image" className='w-[5rem]'/>
+
+
+                    {/* -------------------------------------SearchBar-------------------------------------------- */}
+                    <div className="flex flex-initial w-74 p-4 space-around">
+                        <input type="text" className='input p-3 rounded-lg mr-3 w-[35rem] border-2 border-slate-300 items-center' placeholder='Search...' />
+                        <button href="#" className='button p-3 rounded-lg border-2 border-slate-400 bg-gray-100'>Search</button>
+                    </div>
+
+                    {/* --------------------------Navigation---------------------------- */}
+                    <div className="flex w-[35rem] items-center space-around p-4 space-around" 
+                    style={{marginLeft: "43px",marginRight:" 45px"}}
+                    >
+                        <div href="#" className="text-black mx-8 cursor-pointer">Home</div>
+                        <div href="#" className="text-black mx-8 cursor-pointer">Link1</div>
+                        <div href="#" className="text-black mx-8 cursor-pointer">Link1</div>
+                        <div href="#" className="text-black mx-8 cursor-pointer">Link1</div>
+                        <div href="#" className="text-black mx-8 cursor-pointer">Link1</div>
+                    </div>
+
+                    {/* ----------------LogOut---------------- */}
+                    <div className="bg-blue-500 p-4 rounded-full bg-slate-400">
+                        <button href="#" className="text-white">Sign Out</button>
+                    </div>
+
+                </div>
+            </div>
+
+
+        </div>
+    )
+}
+
+export default Navbar
